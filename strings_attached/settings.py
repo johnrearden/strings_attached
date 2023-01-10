bca32437166debe926d5d1851d956c9a71eef9b4
John Rearden <reardenjohn@gmail.com>
2023-01-10 19:13:23 +0000

    Set up Heroku postgres database, run migrations, deployment commit

--- strings_attached/settings.py
+++ strings_attached/settings.py
@@ -99,17 +99,15 @@ MESSAGE_TAGS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
 DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
     }
-"""
-DATABASES = {
+""" DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+    } """
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
