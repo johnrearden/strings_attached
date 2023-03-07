597b4266cd31a948034bf26c9af9e6bda01c3da7
John Rearden <reardenjohn@gmail.com>
2023-03-07 21:03:34 +0000

    Add missing errors directory for custom 404 page

--- strings_attached/urls.py
+++ strings_attached/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import settings
 from django.conf.urls.static import static
+from . import settings
+from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
