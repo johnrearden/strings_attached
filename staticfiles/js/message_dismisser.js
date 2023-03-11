cbab07cd6ab47d82a9f346f247c554ca28f45e93
John Rearden <reardenjohn@gmail.com>
2023-03-11 12:10:47 +0000

    Remove staticfiles directory from local project

--- staticfiles/js/message_dismisser.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef */
-setTimeout(() => {
-  const messages = document.getElementById('msg');
-  if (messages) {
-    const alert = new bootstrap.Alert(messages);
-    alert.close();
-  }
-}, 2000);
