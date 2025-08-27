eafc5ebb6159cc71eb287bc6d33d1008de885a96
John Rearden <reardenjohn@gmail.com>
2025-08-27 17:58:04 +0100

    reset env.example.py

--- env.example.py
+++ env.example.py
@@ -1,14 +1,15 @@
 
 import os
 
-os.environ["DATABASE_URL"] = ""
-os.environ["SECRET_KEY"] = ""
+#os.environ["DATABASE_URL"] = "postgres://u1ncnbb3m5i2ef:p86ed661c3ec3db76b57885b919b92c4118ec6fe81a47b79eabdbd760c73ae858@c458vgefr82mn8.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d9db1a7gucfcrd"
+os.environ["DATABASE_URL"] = "postgres://john:I%$!Xp1A5@2xCAd%I4C4M!NG@rojosample.net:5432/strings-attached"
+os.environ["SECRET_KEY"] = "ye(shu12$6la4r$u+$9get5c$xi6m^0g_926nd6@vid+*+6kgr"
 os.environ["DEBUG"] = 'True'
-os.environ["EMAIL_APP_PASSWORD"] = ""
-os.environ["EMAIL_APP_USER"] = ""
+os.environ["EMAIL_APP_PASSWORD"] = "qcnzkspjljlbcqxz"
+os.environ["EMAIL_APP_USER"] = "reardenjohn@gmail.com"
 os.environ["STRIPE_PUBLIC_KEY"] = ""
 os.environ["STRIPE_PRIVATE_KEY"] = ""
 os.environ['STRIPE_WEBHOOK_SECRET'] = ""
 os.environ['BASE_URL'] = 'http://localhost:8000'
-os.environ['AWS_ACCESS_KEY_ID'] = ""
-os.environ['AWS_SECRET_ACCESS_KEY'] = ""
+os.environ['AWS_ACCESS_KEY_ID'] = "AKIARRKGULXJHAAPTKTM"
+os.environ['AWS_SECRET_ACCESS_KEY'] = "0a1Uwexob36MSYhycQwG1LXF0RkDdoO6kOvm"
